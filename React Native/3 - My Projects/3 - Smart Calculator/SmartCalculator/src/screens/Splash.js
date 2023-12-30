9384001038f599e67fad136c3d4715c17d9bb727
Ansari Junaid <ansarijunaid957@gmail.com>
2023-12-31 01:00:12 +0530

    added custom splash screen dos

--- React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/Splash.js
+++ React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/Splash.js
@@ -1,36 +1,68 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import { Button, StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// const Splash = (props) => {
+
+//   const navigateToHome = () => {
+//     props.navigation.navigate('Home')
+//   }
+
+//   const navigateToAbout = () => {
+//     props.navigation.navigate('About')
+//   }
+
+//   return (
+//     <View>
+//       <Text style={{ color: "black",fontSize: 40,textAlign: "center",
+//       }} >
+//         Splash
+//       </Text>
+
+//       {/* Navigation Home Button */}
+//       <View style={{ marginTop: 10 }}>
+//         <Button title="Home" color="#841584"
+//           onPress={navigateToHome}
+//         />
+//       </View>
+
+//       {/* Navigation About Button */}
+//       <View style={{ marginTop: 10 }}>
+//         <Button title="About" color="#841584"
+//           onPress={navigateToAbout}
+//         />
+//       </View>
+
+//     </View>
+//   )
+// }
+
+// export default Splash
+
+
+
+
+
+import { Button, Image, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+
+
+const logo = '../../assets/images/logo.png'
 
 const Splash = (props) => {
 
-  const navigateToHome = () => {
-    props.navigation.navigate('Home')
-  }
-
-  const navigateToAbout = () => {
-    props.navigation.navigate('About')
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      props.navigation.replace('Home')
+    }, 2000)
+  }, [])
 
   return (
-    <View>
-      <Text style={{ color: "black",fontSize: 40,textAlign: "center",
-      }} >
-        Splash
-      </Text>
+    <View style={{ backgroundColor: "black", flex: 1, justifyContent: "center", alignItems: "center" }}>
 
-      {/* Navigation Home Button */}
-      <View style={{ marginTop: 10 }}>
-        <Button title="Home" color="#841584"
-          onPress={navigateToHome}
-        />
-      </View>
-
-      {/* Navigation About Button */}
-      <View style={{ marginTop: 10 }}>
-        <Button title="About" color="#841584"
-          onPress={navigateToAbout}
-        />
-      </View>
+      <Image
+        source={require(logo)}
+      // style={{height:40 , width:40}}
+      />
 
     </View>
   )
