efc8802974de18ca3343398eef9f7d90ac63878c
Ansari Junaid <ansarijunaid957@gmail.com>
2023-12-31 02:28:28 +0530

    added animation on splash screen

--- React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/Splash.js
+++ React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/Splash.js
@@ -41,12 +41,11 @@
 
 
 
-
-import { Button, Image, StyleSheet, Text, View } from 'react-native'
+import { View, Text, Image, Animated } from 'react-native'
 import React, { useEffect } from 'react'
 
-
 const logo = '../../assets/images/logo.png'
+const miniLogo = '../../assets/images/miniLogo.png'
 
 const Splash = (props) => {
 
@@ -56,15 +55,71 @@ const Splash = (props) => {
     }, 2000)
   }, [])
 
+  const fadeAnim = new Animated.Value(0)
+  const rotateAnim = new Animated.Value(0)
+
+  useEffect(() => {
+    const fadeIn = Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    });
+
+    const rotate = Animated.timing(rotateAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    });
+
+    Animated.sequence([fadeIn, rotate]).start();
+  }, [fadeAnim, rotateAnim]);
+
+  const rotateValue = rotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  })
+
+
   return (
-    <View style={{ backgroundColor: "black", flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{
+      backgroundColor: "black",
+      flex: 1,
+      borderColor: "red",
+      // padding: 10,
+      // borderWidth: 1
+    }}>
+      <View style={{
+        // borderColor: "white",
+        // borderWidth: 1,
+        flex: 6,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+      >
+        <Image source={require(logo)} />
+      </View>
 
-      <Image
-        source={require(logo)}
-      // style={{height:40 , width:40}}
-      />
+      <Animated.View style={[
+        {
+          // borderColor: "white",
+          // borderWidth: 1,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 40
+        },
+        {
+          opacity: fadeAnim,
+          transform: [{ rotate: rotateValue }]
+        }
+      ]}>
 
-    </View>
+        <Image source={require(miniLogo)} />
+
+      </Animated.View>
+
+
+    </View >
   )
 }
 
