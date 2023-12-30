4fea68990dec7c9227d2184087c58909226415a5
Ansari Junaid <ansarijunaid957@gmail.com>
2023-12-31 00:22:11 +0530

    added docs to add navigation

--- React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/About.js
+++ React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/About.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const About = () => {
+const About = (props) => {
+
+  const navigateToSplash = () => {
+    props.navigation.navigate('Splash')
+  }
+
+  const navigateToHome = () => {
+    props.navigation.navigate('Home')
+  }
   return (
     <View>
-      <Text>About</Text>
+      <Text
+        style={{ color: "black", fontSize: 40, textAlign: "center" }}
+      >About
+      </Text>
+
+      {/* Navigation to Splash Screen */}
+      <View style={{ marginTop: 10 }}>
+        <Button title='Splash'
+          onPress={navigateToSplash}
+        />
+      </View>
+
+      {/* Navigation to Home Screen */}
+      <View style={{ marginTop: 10 }}>
+        <Button title='Home'
+          onPress={navigateToHome}
+        />
+      </View>
+
     </View>
   )
 }
 
 export default About
-
-const styles = StyleSheet.create({})
--- React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/Home.js
+++ React Native/3 - My Projects/3 - Smart Calculator/SmartCalculator/src/screens/Home.js
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+const Home = (props) => {
+
+  const navigateToSplash = () => {
+    props.navigation.navigate('Splash')
+  }
+
+  const navigateToAbout = () => {
+    props.navigation.navigate('About')
+  }
   return (
     <View>
-      <Text style={{}}>Home</Text>
+      <Text
+        style={{ color: "black", fontSize: 40, textAlign: "center" }}
+      >Home
+      </Text>
+
+      {/* Navigate to Splash Screen  */}
+      <View style={{ marginTop: 10 }}>
+        <Button title='Splash'
+          onPress={navigateToSplash}
+        />
+      </View>
+
+      {/* Navigate to About Screen  */}
+      <View style={{ marginTop: 10 }}>
+        <Button title='About'
+          onPress={navigateToAbout}
+        />
+      </View>
+
     </View>
   )
 }
